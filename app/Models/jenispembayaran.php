d87cce4914a8a9e0c6d0b32a59267284d1581fde
albirrafebryo <birrawng@gmail.com>
2025-07-26 21:50:38 +0700

    ppp

--- app/Models/jenispembayaran.php
+++ app/Models/jenispembayaran.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class jenispembayaran extends Model
+class JenisPembayaran extends Model
 {
      protected $table = 'jenispembayarans';
      protected $guarded = [];
