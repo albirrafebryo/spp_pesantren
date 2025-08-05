b6261205f750acac926353d22d41b3d71c356827
albirrafebryo <birrawng@gmail.com>
2025-08-05 19:41:46 +0700

    pp

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Auth::loginUsingId(id:2);
+        // Auth::loginUsingId(id:2);
     }
 }
